3c82982883d069a00070931d4c84b099f2a0d9c2
Fernando Candiani <candianirfernando@gmail.com>
2024-04-18 16:55:37 -0300

    Adicionando as foto e nome dos usuário no story

--- facebook/lib/componentes/area_estoria.dart
+++ facebook/lib/componentes/area_estoria.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook/componentes/imagem_perfil.dart';
+import 'package:facebook/dados/dados.dart';
 import 'package:facebook/modelos/modelos.dart';
+import 'package:facebook/uteis/paleta_cores.dart';
 import 'package:flutter/material.dart';
 
 class AreaStoria extends StatelessWidget {
@@ -20,9 +23,24 @@ class AreaStoria extends StatelessWidget {
       child: ListView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
         scrollDirection: Axis.horizontal,
-        itemCount: estorias.length,
+        itemCount: 1 + estorias.length,
         itemBuilder: (context, indice) {
-          Estoria estoria = estorias[indice];
+          if (indice == 0) {
+            Estoria estoriaUsuario = Estoria(
+              usuario: usuarioAtual,
+              urlImagem: usuarioAtual.urlImagem,
+            );
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: CartaoEstoria(
+                adicionarEstoria: true,
+                estoria: estoriaUsuario,
+              ),
+            );
+          }
+
+          Estoria estoria = estorias[indice - 1];
 
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 4),
@@ -36,7 +54,12 @@ class AreaStoria extends StatelessWidget {
 
 class CartaoEstoria extends StatelessWidget {
   final Estoria estoria;
-  const CartaoEstoria({super.key, required this.estoria});
+  final bool adicionarEstoria;
+  const CartaoEstoria({
+    super.key,
+    required this.estoria,
+    this.adicionarEstoria = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +72,50 @@ class CartaoEstoria extends StatelessWidget {
             height: double.infinity,
             width: 110,
             fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          height: double.infinity,
+          width: 110,
+          decoration: BoxDecoration(
+            gradient: PaletaCores.degradeEstoria,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        Positioned(
+          top: 8,
+          left: 8,
+          child: adicionarEstoria
+              ? Container(
+                  height: 40,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: const Icon(Icons.add),
+                    iconSize: 30,
+                    color: PaletaCores.azulFacebook,
+                    onPressed: () {},
+                  ),
+                )
+              : ImagemPerfil(
+                  urlImagem: estoria.usuario.urlImagem,
+                  foiVisualizado: estoria.foiVisualizado,
+                ),
+        ),
+        Positioned(
+          bottom: 8,
+          left: 8,
+          right: 8,
+          child: Text(
+            adicionarEstoria ? "Criar Story" : estoria.usuario.nome,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
           ),
         ),
       ],
