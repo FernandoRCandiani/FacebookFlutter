b8927f0b3a90a5bcfefc9978b6c53a0b26b166f9
Fernando Candiani <candianirfernando@gmail.com>
2024-05-14 17:12:43 -0300

    Criando lista de contatos

--- facebook/lib/telas/home.dart
+++ facebook/lib/telas/home.dart
@@ -2,11 +2,13 @@ import 'package:facebook/componentes/area_criar_postagem.dart';
 import 'package:facebook/componentes/area_estoria.dart';
 import 'package:facebook/componentes/botao_circulo.dart';
 import 'package:facebook/componentes/cartao_postagem.dart';
+import 'package:facebook/componentes/lista_contatos.dart';
 import 'package:facebook/dados/dados.dart';
 import 'package:facebook/modelos/modelos.dart';
 import 'package:facebook/uteis/paleta_cores.dart';
 import 'package:facebook/uteis/responsivo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:line_icons/line_icons.dart';
 
 class Home extends StatefulWidget {
@@ -89,24 +91,46 @@ class HomeDesktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverPadding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
-          sliver: SliverToBoxAdapter(
-            child: AreaStoria(usuario: usuarioAtual, estorias: estorias),
+    return Row(
+      children: [
+        Flexible(
+          flex: 2,
+          child: Container(
+            color: Colors.red,
           ),
         ),
-        SliverToBoxAdapter(
-          child: AreaCriarPostagem(usuario: usuarioAtual),
+        const Spacer(),
+        Flexible(
+          flex: 5,
+          child: CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+                sliver: SliverToBoxAdapter(
+                  child: AreaStoria(usuario: usuarioAtual, estorias: estorias),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: AreaCriarPostagem(usuario: usuarioAtual),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, indice) {
+                    Postagem postagem = postagens[indice];
+                    return CartaoPostagem(postagem: postagem);
+                  },
+                  childCount: postagens.length,
+                ),
+              ),
+            ],
+          ),
         ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (context, indice) {
-              Postagem postagem = postagens[indice];
-              return CartaoPostagem(postagem: postagem);
-            },
-            childCount: postagens.length,
+        const Spacer(),
+        Flexible(
+          flex: 2,
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: ListaContatos(usuarios: usuariosOnline,),
           ),
         ),
       ],
